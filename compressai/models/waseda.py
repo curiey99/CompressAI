52837c36116ba072831c2f375b50c0b9f3041a2b
curieyoon <curie.yoon@gmail.com>
2022-10-12 04:40:45 +0900

    waseda

--- compressai/models/waseda.py
+++ compressai/models/waseda.py
@@ -40,6 +40,7 @@ from compressai.layers import (
 from compressai.registry import register_model
 
 from .google import JointAutoregressiveHierarchicalPriors
+from .google import MeanScaleHyperprior
 
 
 @register_model("cheng2020-anchor")
@@ -114,24 +115,11 @@ class Cheng2020Anchor(JointAutoregressiveHierarchicalPriors):
 
 
 @register_model("cheng2020-attn")
-class Cheng2020Attention(Cheng2020Anchor):
-    """Self-attention model variant from `"Learned Image Compression with
-    Discretized Gaussian Mixture Likelihoods and Attention Modules"
-    <https://arxiv.org/abs/2001.01568>`_, by Zhengxue Cheng, Heming Sun, Masaru
-    Takeuchi, Jiro Katto.
-
-    Uses self-attention, residual blocks with small convolutions (3x3 and 1x1),
-    and sub-pixel convolutions for up-sampling.
-
-    Args:
-        N (int): Number of channels
-    """
-
+class Cheng2020Attention(MeanScaleHyperprior):
     def __init__(self, N=192, **kwargs):
-        super().__init__(N=N, **kwargs)
-
+        super().__init__(N=N, M=N, **kwargs)
         self.g_a = nn.Sequential(
-            ResidualBlockWithStride(3, N, stride=2),
+            ResidualBlockWithStride(256, N, stride=2),
             ResidualBlock(N, N),
             ResidualBlockWithStride(N, N, stride=2),
             AttentionBlock(N),
@@ -152,5 +140,98 @@ class Cheng2020Attention(Cheng2020Anchor):
             ResidualBlock(N, N),
             ResidualBlockUpsample(N, N, 2),
             ResidualBlock(N, N),
-            subpel_conv3x3(N, 3, 2),
+            subpel_conv3x3(N, 256, 2),
         )
+
+        # self h_a, h_s: same as Cheng2020Anchor
+        self.h_a = nn.Sequential(
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N, stride=2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N, stride=2),
+        )
+
+        self.h_s = nn.Sequential(
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            subpel_conv3x3(N, N, 2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N * 3 // 2),
+            nn.LeakyReLU(inplace=True),
+            subpel_conv3x3(N * 3 // 2, N * 3 // 2, 2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N * 3 // 2, N * 2),
+        )
+
+    @classmethod
+    def from_state_dict(cls, state_dict):
+        """Return a new model instance from `state_dict`."""
+        N = state_dict["g_a.0.conv1.weight"].size(0)
+        net = cls(N)
+        net.load_state_dict(state_dict)
+        return net
+
+
+
+"""
+Original Attention Model
+
+
+
+    
+    Self-attention model variant from `"Learned Image Compression with
+    Discretized Gaussian Mixture Likelihoods and Attention Modules"
+    <https://arxiv.org/abs/2001.01568>`_, by Zhengxue Cheng, Heming Sun, Masaru
+    Takeuchi, Jiro Katto.
+
+    Uses self-attention, residual blocks with small convolutions (3x3 and 1x1),
+    and sub-pixel convolutions for up-sampling.
+
+    Args:
+        N (int): Number of channels
+  
+    def __init__(self, N=192, **kwargs):
+        super().__init__(N=N, **kwargs)
+
+        self.g_a = nn.Sequential(
+            ResidualBlockWithStride(256, N, stride=2),
+            ResidualBlock(N, N),
+            ResidualBlockWithStride(N, N, stride=2),
+            AttentionBlock(N),
+            ResidualBlock(N, N),
+            ResidualBlockWithStride(N, N, stride=2),
+            ResidualBlock(N, N),
+            conv3x3(N, N, stride=2),
+            AttentionBlock(N),
+        )
+
+        self.g_s = nn.Sequential(
+            AttentionBlock(N),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            AttentionBlock(N),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            ResidualBlock(N, N),
+            subpel_conv3x3(N, 256, 2),
+        )
+
+
+"""
+
+
+"""
+@register_model("cheng2020-attn-woctx")
+class Cheng2020AttentionwoCTX(JointAutoregressiveHierarchicalPriors):
+    def __init__(self, N=192, **kwargs):
+        super().__init__(N=N, **kwargs)
+
+
+"""
