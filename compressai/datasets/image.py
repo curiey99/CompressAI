ba7fc191c40200d4193542899d29f4c00206e02a
curieyoon <curie.yoon@gmail.com>
2023-01-03 01:20:42 +0900

    debug FFpad crop

--- compressai/datasets/image.py
+++ compressai/datasets/image.py
@@ -263,6 +263,7 @@ class FeatureFolderPad(Dataset):
         assert t.shape[0] == 1 and t.shape[1] == 16
         if self.samples[index].stem[1] == '5':   # p5
             t = interpolate(t, scale_factor=2, mode='bicubic')
+        print(t.shape)
         if self.crop is not None and self.split != 'test':
             tt = torch.empty((1, 16, self.crop, self.crop))
             r = random.randint(0, t.shape[1]-self.crop-1)
